db2c94971f62a92b0ece3c851bcfa3094c0530ed
omar ezzat <omar.m.3zzat@gmail.com>
2026-05-19 03:24:07 +0300

    .

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,17 +12,15 @@ const nodemailer = require("nodemailer");
 
 // ─── Nodemailer transporter ───────────────────────────────────────────────────
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // true for 465, false for 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
-    // Force IPv4
-    family: 4
   }
 });
+
 
 
 transporter.verify((error, success) => {
